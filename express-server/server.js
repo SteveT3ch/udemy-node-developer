c267f3931e27cba9b2d7be65eb85a94ec31b85ec
Steve Peters <steve.t3ch@gmail.com>
2018-08-16 01:56:40 -0400

    Setup script and heroku port

--- express-server/server.js
+++ express-server/server.js
@@ -1,26 +1,52 @@
 const { log } = console;
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next)=>{
+  let now = new Date().toString();
+  let serverLog = `${now}: ${req.method} ${req.url}`;
+  log(serverLog);
+/*  fs.appendFile('server.log', serverLog + '\n', (err)=> {
+    if (err) {
+      log('Unable to log to server');
+    };
+  }); */
+  next();
+})
+
+// app.use((req, res, next) =>{
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
       pageTitle: 'Home Page',
-      welcomeMessage: 'Welcome to the Home Page',
-      currentYear: new Date().getFullYear()
+      welcomeMessage: 'Welcome to the Home Page'
 
     })
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
@@ -31,6 +57,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  log("Listening on port 3000");
+app.listen(port, () => {
+  log(`Listening on port ${port}`);
 });
